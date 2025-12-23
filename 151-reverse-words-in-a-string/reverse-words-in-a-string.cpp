9bf4390434f177d8afa37ba8252f1b61f5e9053b
Isha Patel <isha.patel.cg@gmail.com>
2025-12-23 10:14:07 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (56.49%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,23 +1,54 @@
+// consists of different methods 
+
 class Solution {
 public:
     string reverseWords(string s) {
-        string result = "";
-        int i =s.size()-1;
-        while(i>=0){
-            while (i>=0 && s[i]== ' ')
-                i--;
-
-            if(i<0) break;
-
-            int j=i;
-
-            while(i>=0 && s[i]!=' ')
-                i--;
-                if(!result.empty())
-                    result+=" ";
-                    result+=s.substr(i+1,j-i);
+        reverse(s.begin(),s.end());
+        string ans="";
+        for(int i=0;i<s.length();i++){
+            string revStr="";
+            while (i < s.length() && s[i] == ' ')
+                i++;
+            while(i<s.length() && s[i]!=' '){
+                revStr+=s[i];
+                i++;
+            }
+            reverse(revStr.begin(),revStr.end());
+            if(revStr.length()!=0){
+                if (!ans.empty())
+                    ans += " ";
+                ans+=revStr;
+            }
         }
-
-        return result;
+        return ans.substr();
     }
 };
+
+
+
+
+
+// 2 pointer from end --> method
+// class Solution {
+// public:
+//     string reverseWords(string s) {
+//         string result = "";
+//         int i =s.size()-1;
+//         while(i>=0){
+//             while (i>=0 && s[i]== ' ')
+//                 i--;
+
+//             if(i<0) break;
+
+//             int j=i;
+
+//             while(i>=0 && s[i]!=' ')
+//                 i--;
+//                 if(!result.empty())
+//                     result+=" ";
+//                     result+=s.substr(i+1,j-i);
+//         }
+
+//         return result;
+//     }
+// };
