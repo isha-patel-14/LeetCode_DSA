932da6b21396836580ceeff2e49492c965394ca8
Isha Patel <isha.patel.cg@gmail.com>
2026-01-13 11:38:00 +0530

    Time: 0 ms (100.00%) | Memory: 24.6 MB (49.51%) - LeetSync

--- 2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.cpp
+++ 2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.cpp
@@ -1,15 +1,38 @@
 class Solution {
 public:
     int countPairs(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
         int count=0;
-        int n=nums.size();
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(nums[i]+nums[j]<target && i<j){
-                    count++;
-                }
+        int left=0;
+        int right=nums.size()-1;
+
+        while(left < right) {
+            int currentSum = nums[left] + nums[right];
+            if (currentSum < target) {
+                count+=(right-left);
+                left++;
+            }else{
+                right--;
             }
         }
         return count;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     int countPairs(vector<int>& nums, int target) {
+//         int count=0;
+//         int n=nums.size();
+//         for(int i=0;i<n;i++){
+//             for(int j=i+1;j<n;j++){
+//                 if(nums[i]+nums[j]<target && i<j){
+//                     count++;
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// };
