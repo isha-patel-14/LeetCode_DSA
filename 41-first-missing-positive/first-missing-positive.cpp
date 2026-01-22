561e419fee1b2a9547e38c6ac5f1efdae2fae55a
Isha Patel <isha.patel.cg@gmail.com>
2026-01-22 14:07:46 +0530

    Time: 0 ms (100.00%) | Memory: 55.2 MB (32.04%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -1,15 +1,38 @@
+// Time	-> O(n log n)
+// Space -> O(log n)
+// class Solution {
+// public:
+//     int firstMissingPositive(vector<int>& nums) {
+//         sort(nums.begin(),nums.end());
+//         int minPositive=1;
+//         for(int i=0;i<nums.size();i++){
+//             if(nums[i]==minPositive){
+//                 minPositive++;
+//             }
+//             if(nums[i]>minPositive)
+//                 return minPositive;
+//         }
+//         return minPositive;
+//     }
+// };
+
+
+// Time	-> O(n)
+// Space -> O(1)
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int minPositive=1;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==minPositive){
-                minPositive++;
-            }
-            if(nums[i]>minPositive)
-                return minPositive;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            while (
+                nums[i]>0 && nums[i]<=n && nums[nums[i] - 1] != nums[i]) {
+                    swap(nums[i], nums[nums[i] - 1]);
+                }
         }
-        return minPositive;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1)
+                return i + 1;
+        }
+        return n + 1;
     }
 };
