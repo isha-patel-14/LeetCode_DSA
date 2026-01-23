d03bf8499f807b7213e14f2e8998ae4fab391424
Isha Patel <isha.patel.cg@gmail.com>
2026-01-23 15:22:23 +0530

    Time: 0 ms (100.00%) | Memory: 26.7 MB (41.78%) - LeetSync

--- 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
@@ -1,18 +1,31 @@
 class Solution {
 public:
     string firstPalindrome(vector<string>& words) {
-        for (int i=0;i<words.size();i++){
-            string s=words[i];
-            string rev="";
-
-            for(int j=s.length()-1;j>=0;j--){
-                rev+=s[j];
-            }
-
-            if(s==rev) {
-                return s;
-            }
+        for(int i = 0; i < words.size(); i++){
+            string temp = words[i];
+            reverse(words[i].begin(), words[i].end());
+            if(temp == words[i])
+                return words[i];
         }
         return "";
     }
 };
+
+// class Solution {
+// public:
+//     string firstPalindrome(vector<string>& words) {
+//         for (int i=0;i<words.size();i++){
+//             string s=words[i];
+//             string rev="";
+
+//             for(int j=s.length()-1;j>=0;j--){
+//                 rev+=s[j];
+//             }
+
+//             if(s==rev) {
+//                 return s;
+//             }
+//         }
+//         return "";
+//     }
+// };
