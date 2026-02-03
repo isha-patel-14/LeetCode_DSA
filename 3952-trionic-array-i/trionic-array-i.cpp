946d99a957aeb6e18abb210bd3945a5249d5085b
Isha Patel <isha.patel.cg@gmail.com>
2026-02-03 10:23:27 +0530

    Time: 0 ms (100.00%) | Memory: 27.6 MB (69.85%) - LeetSync

---
+++ 3952-trionic-array-i/trionic-array-i.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isTrionic(vector<int>& nums) {
+        int i=0,n=nums.size();
+        while(i+1<n && nums[i]<nums[i+1]){
+            i++;
+        }
+        int p=i;
+        if (p==0) return false;
+        while(i+1<n && nums[i]>nums[i+1]){
+            i++;
+        }
+        int q=i;
+        if(q==p || q==n-1) return false;
+        while(i+1<n && nums[i]<nums[i+1]){
+            i++;
+        }
+        return (i==n-1);
+    }
+};
