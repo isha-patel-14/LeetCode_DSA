a78539d75f18ff996be0cff768e952a9c5510dfc
Isha Patel <isha.patel.cg@gmail.com>
2026-02-14 09:52:57 +0530

    Time: 6 ms (50.36%) | Memory: 10.1 MB (74.82%) - LeetSync

--- 815-champagne-tower/champagne-tower.cpp
+++ 815-champagne-tower/champagne-tower.cpp
@@ -1,18 +1,32 @@
 class Solution {
 public:
     double champagneTower(int poured, int query_row, int query_glass) {
-        vector<vector<double>> dp(query_row + 2, vector<double>(query_row + 2, 0.0));
-        dp[0][0] = poured;
-        for (int i = 0; i <= query_row; i++) {
-            for (int j = 0; j <= i; j++) {
-                if (dp[i][j] > 1.0) {
-                    double extra = (dp[i][j] - 1.0) / 2.0;
-                    dp[i + 1][j] += extra;
-                    dp[i + 1][j + 1] += extra;
-                    dp[i][j] = 1.0;
-                }
+        vector<double>dp(query_row+2,0.0);
+        dp[0]=poured;
+        for(int i=1;i<=query_row;i++){
+            for(int j=i;j>=0;j--){
+                dp[j+1] += dp[j] = max((dp[j]-1)/2,0.0);
             }
         }
-        return dp[query_row][query_glass];
+        return min(dp[query_glass],1.0);
     }
 };
+
+// class Solution {
+// public:
+//     double champagneTower(int poured, int query_row, int query_glass) {
+//         vector<vector<double>> dp(query_row + 2, vector<double>(query_row + 2, 0.0));
+//         dp[0][0] = poured;
+//         for (int i = 0; i <= query_row; i++) {
+//             for (int j = 0; j <= i; j++) {
+//                 if (dp[i][j] > 1.0) {
+//                     double extra = (dp[i][j] - 1.0) / 2.0;
+//                     dp[i + 1][j] += extra;
+//                     dp[i + 1][j + 1] += extra;
+//                     dp[i][j] = 1.0;
+//                 }
+//             }
+//         }
+//         return dp[query_row][query_glass];
+//     }
+// };
