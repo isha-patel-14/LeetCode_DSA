7165240056d13bb56ab32efd6a16661a2dcf1cb2
Isha Patel <isha.patel.cg@gmail.com>
2026-02-17 11:10:22 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (58.69%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        if(digits.empty())return {};
+        vector<string> mapping={"", "", "abc", "def", "ghi","jkl", "mno", "pqrs", "tuv", "wxyz"};
+        vector<string>result={""};
+        for(int i=0;i<digits.size();i++){
+            vector<string>temp;
+            string letters=mapping[digits[i]-'0'];
+            for(int j=0;j<result.size();j++){
+                for(int k=0;k<letters.size();k++){
+                    temp.push_back(result[j]+letters[k]);
+                }
+            }
+            result=temp;
+        }
+        return result;
+    }
+};
