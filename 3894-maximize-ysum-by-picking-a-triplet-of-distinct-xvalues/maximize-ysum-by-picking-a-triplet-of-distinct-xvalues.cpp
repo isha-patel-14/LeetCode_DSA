b67275c579acae1a6f0fef97c3e194e9d9d3d2f4
Isha Patel <isha.patel.cg@gmail.com>
2026-02-20 15:09:04 +0530

    Time: 107 ms (38.59%) | Memory: 198.4 MB (35.57%) - LeetSync

--- 3894-maximize-ysum-by-picking-a-triplet-of-distinct-xvalues/maximize-ysum-by-picking-a-triplet-of-distinct-xvalues.cpp
+++ 3894-maximize-ysum-by-picking-a-triplet-of-distinct-xvalues/maximize-ysum-by-picking-a-triplet-of-distinct-xvalues.cpp
@@ -1,19 +1,42 @@
 class Solution {
 public:
     int maxSumDistinctTriplet(vector<int>& x, vector<int>& y) {
-        int n=x.size();
-        if(n<3)return -1;
-        unordered_map<int,int>xToY;
-        for(int i=0;i<n;i++){
-            xToY[x[i]]=max(xToY[x[i]],y[i]);
+        int n = x.size();
+        unordered_map<int, int> best;
+        
+        for (int i = 0; i < n; i++) {
+            best[x[i]] = max(best[x[i]], y[i]);
         }
-        if(xToY.size()<3) return -1;
-
-        vector<int>bestY;
-        for(auto& [key,val]:xToY){
-            bestY.push_back(val);
+        
+        if (best.size() < 3) return -1;
+        
+        vector<int> vals;
+        for (auto &p : best) {
+            vals.push_back(p.second);
         }
-        sort(bestY.rbegin(),bestY.rend());
-        return bestY[0]+bestY[1]+bestY[2];
+        
+        sort(vals.begin(), vals.end(), greater<int>());
+        
+        return vals[0] + vals[1] + vals[2];
     }
 };
+
+// class Solution {
+// public:
+//     int maxSumDistinctTriplet(vector<int>& x, vector<int>& y) {
+//         int n=x.size();
+//         if(n<3)return -1;
+//         unordered_map<int,int>xToY;
+//         for(int i=0;i<n;i++){
+//             xToY[x[i]]=max(xToY[x[i]],y[i]);
+//         }
+//         if(xToY.size()<3) return -1;
+
+//         vector<int>bestY;
+//         for(auto& [key,val]:xToY){
+//             bestY.push_back(val);
+//         }
+//         sort(bestY.rbegin(),bestY.rend());
+//         return bestY[0]+bestY[1]+bestY[2];
+//     }
+// };
