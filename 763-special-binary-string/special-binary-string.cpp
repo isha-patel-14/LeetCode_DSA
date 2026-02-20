6465fbc11b09d386c74a184c34cf6a17c6fc670b
Isha Patel <isha.patel.cg@gmail.com>
2026-02-20 09:51:20 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (34.67%) - LeetSync

---
+++ 763-special-binary-string/special-binary-string.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string makeLargestSpecial(string s) {
+        vector<string> parts;
+        int balance = 0, start = 0;
+
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '1') balance++;
+            else balance--;
+
+            if (balance == 0) {
+                string inner = s.substr(start + 1, i - start - 1);
+                parts.push_back("1" + makeLargestSpecial(inner) + "0");
+                start = i + 1;
+            }
+        }
+
+        sort(parts.begin(), parts.end(), greater<string>());
+
+        string result = "";
+        for (string &p : parts) result += p;
+
+        return result;
+    }
+};
