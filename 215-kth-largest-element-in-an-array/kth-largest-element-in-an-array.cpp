c20260a9a684308675aaf7d4f37da7383058473e
Isha Patel <isha.patel.cg@gmail.com>
2026-02-25 10:57:50 +0530

    Time: 32 ms (53.66%) | Memory: 64.7 MB (11.13%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,21 +1,28 @@
-// class Solution {
-// public:
-//     int findKthLargest(vector<int>& nums, int k) {
-        
-//     }
-// };
-
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end());
-        vector<int> que;
-        for(int i = nums.size() - 1; i >= 0; i--){
-            que.push_back(nums[i]);
+        priority_queue<int>pq;
+        for(int i=0;i<nums.size();i++){
+            pq.push(nums[i]);
         }
-        return que[k - 1];
+        for(int i=1;i<k;i++){
+            pq.pop();
+        }
+        return pq.top();
     }
 };
+
+// class Solution {
+// public:
+//     int findKthLargest(vector<int>& nums, int k) {
+//         sort(nums.begin(), nums.end());
+//         vector<int> que;
+//         for(int i = nums.size() - 1; i >= 0; i--){
+//             que.push_back(nums[i]);
+//         }
+//         return que[k - 1];
+//     }
+// };
 
 // class Solution {
 // public:
