fac9d5ad74e0face5c83f96be38fc862c54dbd06
Isha Patel <isha.patel.cg@gmail.com>
2026-03-11 10:41:49 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (61.28%) - LeetSync

---
+++ 2663-distribute-money-to-maximum-children/distribute-money-to-maximum-children.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int distMoney(int money, int children) {
+        if(money<children) return -1;
+        int ans=0;
+        money-=children;
+        while(money>=7 && children>0){
+            money-=7;
+            ans++;
+            children--;
+        }
+        if(ans){
+            if(children==0 && money>0) ans--;
+            if(children==1 && money==3) ans--;
+        }
+        return ans;
+    }
+};
