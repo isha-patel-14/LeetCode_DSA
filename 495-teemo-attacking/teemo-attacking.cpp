d637a1a6afcd5abd33724f198f8c700109d8f027
Isha Patel <isha.patel.cg@gmail.com>
2026-03-17 11:11:10 +0530

    Time: 0 ms (100.00%) | Memory: 29.5 MB (93.54%) - LeetSync

--- 495-teemo-attacking/teemo-attacking.cpp
+++ 495-teemo-attacking/teemo-attacking.cpp
@@ -2,14 +2,29 @@ class Solution {
 public:
     int findPoisonedDuration(vector<int>& timeSeries, int duration) {
         int count=0;
-        for(int i=1;i<timeSeries.size();i++){
-            if(timeSeries[i]-timeSeries[i-1]<duration){
-                count+=timeSeries[i]-timeSeries[i-1];
+        for(int i=0;i<timeSeries.size()-1;i++){
+            if(timeSeries[i+1]-timeSeries[i]<duration){
+                count+=timeSeries[i+1]-timeSeries[i];
             }else{
                 count+=duration;
             }
         }
-        count+=duration;
-        return count;
+        return count+=duration;
     }
 };
+
+// class Solution {
+// public:
+//     int findPoisonedDuration(vector<int>& timeSeries, int duration) {
+//         int count=0;
+//         for(int i=1;i<timeSeries.size();i++){
+//             if(timeSeries[i]-timeSeries[i-1]<duration){
+//                 count+=timeSeries[i]-timeSeries[i-1];
+//             }else{
+//                 count+=duration;
+//             }
+//         }
+//         count+=duration;
+//         return count;
+//     }
+// };
