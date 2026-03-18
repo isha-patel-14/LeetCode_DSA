fca8c68cb34a69d37ccf0fa9224bd755ef2eb953
Isha Patel <isha.patel.cg@gmail.com>
2026-03-18 11:02:57 +0530

    Time: 35 ms (61.57%) | Memory: 36.4 MB (69.44%) - LeetSync

---
+++ 3163-subarrays-distinct-element-sum-of-squares-i/subarrays-distinct-element-sum-of-squares-i.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int sumCounts(vector<int>& nums) {
+        int n=nums.size(),result=0;
+        for(int i=0;i<n;i++){
+            unordered_set<int>st;
+            for(int j=i;j<n;j++){
+                st.insert(nums[j]);
+                int size=st.size();
+                result+=size*size;
+            }
+        }
+        return result;
+    }
+};
